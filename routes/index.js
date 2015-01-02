bfa92efe24fbc65a10c13cdf4f1168d642f3f7aa
Lonny Gomes <code@lonnygomes.com>
2015-01-01 19:33:23 -0500

    Upgraded to express 4
    Incorporated output from the express 4 generator and changes some of the
    routing. Also added a generic HTML file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-
+/*jslint node: true */
 /*
  * GET home page.
  */
+var express = require('express');
+var router = express.Router();
+var path = require('path');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+/* GET home page. */
+router.get('/', function (req, res) {
+    'use strict';
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+module.exports = router;
